89db94ee24d34392542a1b5f9a9bbc9b745de153
endriwmsi <endriwmsi@gmail.com>
2025-09-30 14:16:14 -0300

    chore: implement DashboardPage component and add Toaster in RootLayout; create CPF and CNPJ validation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,36 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getValidDomains = () => {
+  const domains = [
+    "gmail.com",
+    "outlook.com",
+    "yahoo.com",
+    "hotmail.com",
+    "icloud.com",
+    "gsrocket.com.br",
+  ];
+
+  return domains;
+};
+
+export const normalizeName = (name: string) => {
+  return name
+    .trim()
+    .replace(/\s+g/g, " ")
+    .replace(/[^a-zA-Z\s'-]/g, "")
+    .replace(/\b\w/g, (char) => char.toUpperCase());
+};
+
+export const getUserInitials = (name: string) => {
+  const nameParts = name.trim().split(/\s+/);
+  const firstInitial = nameParts[0] ? nameParts[0][0].toUpperCase() : "";
+  const lastInitial = nameParts[nameParts.length - 1]
+    ? nameParts[nameParts.length - 1][0].toUpperCase()
+    : "";
+  return nameParts.length > 1 ? `${firstInitial}${lastInitial}` : firstInitial;
+};
